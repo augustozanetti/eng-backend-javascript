ad0c84227a2c9da701fa99f2f32891bb3d309704
augustozanetti <augusto.zanetti@ze.delivery>
2022-10-24 12:55:30 -0300

    feat: add graphql schema

---
+++ src/application/graphql/schema.ts
@@ -0,0 +1,62 @@
+export const typeDefs = `#graphql
+  type Currency {
+    code: String!
+  }
+  
+  type Money {
+    amount: Int
+    currency: String!
+  }
+
+  type Quote {
+    id: ID!
+    currency: String!
+    value: Float!
+  }
+
+  type Product {
+    id: ID!
+    name: String
+    money(currencies: [String!]): [Money!]!
+  }
+
+  input AddProductInput {
+    name: String!
+    value: Int!
+  }
+
+  input UpdateProductInput {
+    id: ID!
+    name: String!
+    value: Int!
+  }
+
+  input AddQuoteInput {
+    currency: String!
+    value: Float!
+  }
+
+  input UpdateQuoteInput {
+    id: ID!
+    currency: String!
+    value: Float!
+  }
+
+  type Query {
+    currencies: [Currency]!
+    quotes: [Quote]!
+    products: [Product]!
+  }
+
+  
+  type Mutation {
+    addCurrency(code: String!): Currency
+    addQuote(input: AddQuoteInput!): Quote
+    updateQuote(input: UpdateQuoteInput!): Quote
+    addProduct(input: AddProductInput!): Product
+    updateProduct(input: UpdateProductInput!): Product
+    deleteProduct(id: ID!): Boolean
+    deleteCurrency(code: String!): Boolean
+    deleteQuote(id: ID!): Boolean
+  }
+`;
