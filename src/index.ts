60f3a84ab9a3a23365f1c5f29578e327f5f00105
augustozanetti <augusto.zanetti@ze.delivery>
2022-10-24 12:56:29 -0300

    feat: add project entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("start");
+import "reflect-metadata";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { start } from "@/application/graphql/index";
+import { AppDataSource } from "@/infra/database/config/index";
+
+const initialize = async () => {
+  try {
+    await AppDataSource.initialize();
+
+    start();
+  } catch (error) {
+    console.error("error to set up project", error);
+    process.exit(1);
+  }
+};
+
+initialize();
